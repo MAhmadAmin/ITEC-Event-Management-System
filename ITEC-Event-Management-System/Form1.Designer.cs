d3da24104eae8638abd5beeed7991ba070b90878
MAhmadAmin <ahmadamin1005@gmail.com>
2025-02-28 22:06:10 +0500

    Login Page Halfway

--- ITEC-Event-Management-System/Form1.Designer.cs
+++ ITEC-Event-Management-System/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ITEC_Event_Management_System
 {
-    partial class Form1
+    partial class Login
     {
         /// <summary>
         /// Required designer variable.
@@ -28,33 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.LeftPanel = new System.Windows.Forms.Panel();
+            this.LeftPicture = new System.Windows.Forms.PictureBox();
+            this.TopPanel = new System.Windows.Forms.Panel();
+            this.ITECHeading = new System.Windows.Forms.Label();
+            this.UsernameTextBox = new System.Windows.Forms.TextBox();
+            this.LeftPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.LeftPicture)).BeginInit();
+            this.TopPanel.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // LeftPanel
             // 
-            this.button1.Location = new System.Drawing.Point(402, 203);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(102, 50);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.LeftPanel.Controls.Add(this.LeftPicture);
+            this.LeftPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.LeftPanel.Location = new System.Drawing.Point(0, 0);
+            this.LeftPanel.Name = "LeftPanel";
+            this.LeftPanel.Size = new System.Drawing.Size(668, 1112);
+            this.LeftPanel.TabIndex = 0;
             // 
-            // Form1
+            // LeftPicture
+            // 
+            this.LeftPicture.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.LeftPicture.Image = global::ITEC_Event_Management_System.Properties.Resources.FrontPageImage;
+            this.LeftPicture.Location = new System.Drawing.Point(0, 0);
+            this.LeftPicture.Margin = new System.Windows.Forms.Padding(0);
+            this.LeftPicture.Name = "LeftPicture";
+            this.LeftPicture.Size = new System.Drawing.Size(668, 1112);
+            this.LeftPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.LeftPicture.TabIndex = 0;
+            this.LeftPicture.TabStop = false;
+            // 
+            // TopPanel
+            // 
+            this.TopPanel.Controls.Add(this.UsernameTextBox);
+            this.TopPanel.Controls.Add(this.ITECHeading);
+            this.TopPanel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.TopPanel.Location = new System.Drawing.Point(867, 0);
+            this.TopPanel.Name = "TopPanel";
+            this.TopPanel.Size = new System.Drawing.Size(1101, 1112);
+            this.TopPanel.TabIndex = 1;
+            // 
+            // ITECHeading
+            // 
+            this.ITECHeading.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ITECHeading.AutoSize = true;
+            this.ITECHeading.Font = new System.Drawing.Font("Arial Black", 20.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ITECHeading.ForeColor = System.Drawing.Color.DarkOrange;
+            this.ITECHeading.Location = new System.Drawing.Point(335, 46);
+            this.ITECHeading.Name = "ITECHeading";
+            this.ITECHeading.Size = new System.Drawing.Size(555, 95);
+            this.ITECHeading.TabIndex = 0;
+            this.ITECHeading.Text = "ITEC Manager";
+            // 
+            // UsernameTextBox
+            // 
+            this.UsernameTextBox.Location = new System.Drawing.Point(390, 432);
+            this.UsernameTextBox.Name = "UsernameTextBox";
+            this.UsernameTextBox.Size = new System.Drawing.Size(524, 38);
+            this.UsernameTextBox.TabIndex = 1;
+            this.UsernameTextBox.Text = "sdgdg";
+            // 
+            // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Event Manager";
+            this.ClientSize = new System.Drawing.Size(1968, 1112);
+            this.Controls.Add(this.TopPanel);
+            this.Controls.Add(this.LeftPanel);
+            this.MinimumSize = new System.Drawing.Size(2000, 1200);
+            this.Name = "Login";
+            this.Text = "ITEC Event Manager";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.LeftPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.LeftPicture)).EndInit();
+            this.TopPanel.ResumeLayout(false);
+            this.TopPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel LeftPanel;
+        private System.Windows.Forms.PictureBox LeftPicture;
+        private System.Windows.Forms.Panel TopPanel;
+        private System.Windows.Forms.Label ITECHeading;
+        private System.Windows.Forms.TextBox UsernameTextBox;
     }
 }
 
--- ITEC-Event-Management-System/Form1.cs
+++ ITEC-Event-Management-System/Form1.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace ITEC_Event_Management_System
 {
-    public partial class Form1 : Form
+    public partial class Login : Form
     {
-        public Form1()
+        public Login()
         {
             InitializeComponent();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
